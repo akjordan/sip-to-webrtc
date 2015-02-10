6d7504caa1581a462e5eca491421b8bd07be83b9
Andrew <andrew.k.jordan@gmail.com>
2015-02-10 14:55:40 -0800

    adds twilio client to /twilio, adds logic to redirect to twilio after login

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,4 +1,7 @@
 class TwilioController < ApplicationController
   def index
+    capability = Twilio::Util::Capability.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+    capability.allow_client_incoming current_user.id
+   @token = capability.generate()
   end
 end
