483e922bcf7be106fe49b4a14c5202c9fe89e062
Andrew <andrew.k.jordan@gmail.com>
2015-02-09 16:58:54 -0800

    Tweaks to the homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,11 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
-    #@users = User.all
+    @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+   @user = User.find(params[:id])
     unless @user == current_user
       redirect_to :back, :alert => "Access denied."
     end
