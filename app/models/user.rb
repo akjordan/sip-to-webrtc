0b1edee660ba574e5a08698ecbcf9eb33abc024e
Andrew <andrew.k.jordan@gmail.com>
2015-02-09 18:06:21 -0800

    wires up new fields for models, adds webhookable concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def name
+    [first_name, last_name].compact.join(' ')
+  end
+  
 end
