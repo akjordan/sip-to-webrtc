58a5e274331473f60ddd7a0ddbe179d27f7d7c8f
Andrew <andrew.k.jordan@gmail.com>
2015-02-11 19:38:16 -0800

    Twilio account creation happens just after user account creation, SIP still doesn't stil work

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post 'incoming' => 'twilio#incoming'
 
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :users
 end
