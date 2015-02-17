47895b8cf99e4d97f37a564e467cfe10cdd83322
Andrew <andrew.k.jordan@gmail.com>
2015-02-17 15:41:55 +0000

    I think you're supposed to render partials for this kind of thing

--- app/controllers/webrtc_agents_controller.rb
+++ app/controllers/webrtc_agents_controller.rb
@@ -12,6 +12,18 @@ class WebrtcAgentsController < ApplicationController
       @domain = @agent.sip_domain
     end
 
+    if @agent.ip_acl.nil?
+      @ip_acl_label = "Create IPACL"
+    else
+      @ip_acl_label = "Configure IPACL"
+    end
+
+    if @agent.auth_acl.nil?
+      @acl_label = "Create ACL"
+    else
+      @acl_label = "Configure ACL"
+    end
+
     capability = Twilio::Util::Capability.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
     capability.allow_client_incoming current_user.id
    @token = capability.generate()
