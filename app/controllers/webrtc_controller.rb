2c270924f72bbb4c1caae67df274e5e02525b0a6
Andrew <andrew.k.jordan@gmail.com>
2015-03-14 19:26:27 +0000

    adds UI spit and polish

--- app/controllers/webrtc_controller.rb
+++ app/controllers/webrtc_controller.rb
@@ -27,6 +27,8 @@ class WebrtcController < ApplicationController
       @auth_acl = @client.account.sip.credential_lists.get(@agent.auth_acl) 
     end
 
+    @agent_name = current_user.name
+
     capability = Twilio::Util::Capability.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
     capability.allow_client_incoming current_user.id
    @token = capability.generate()
