f28b3c271f32df704a2d46daadb7c07c8dba3e66
Andrew <andrew.k.jordan@gmail.com>
2015-03-15 12:52:18 +0000

    adds force https for better webrtc experience

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
+
   def after_sign_in_path_for(resource)
     webrtc_path
   end
