9309e6a7ab9faf60dfbb6219e0cbeb97f7c6a64b
Andrew <andrew.k.jordan@gmail.com>
2015-03-16 13:03:45 +0000

    more sendgrid fiddling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['DOMAIN_NAME']
+    :domain         => ENV['SENDGRID_DOMAIN']
   }
   # ActionMailer Config
   config.action_mailer.delivery_method = :smtp
