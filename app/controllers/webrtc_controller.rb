e182fad9f377052c3e10bb182dfa2bca67cb72ec
Andrew <andrew.k.jordan@gmail.com>
2015-05-06 15:51:58 +0100

    partial refactor of webrtc_controller

--- app/controllers/webrtc_controller.rb
+++ app/controllers/webrtc_controller.rb
@@ -3,7 +3,9 @@ class WebrtcController < ApplicationController
 
   def index
     @agent = current_user
-    @client = Twilio::REST::Client.new(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
+    @agent_name = current_user.name
+    @token = capability_token
+    @client = create_rest_client
 
     if @agent.phone_number.nil? || @agent.sip_domain.nil?
       @number =  'agent not created'
@@ -14,24 +16,31 @@ class WebrtcController < ApplicationController
     end
 
     if @agent.ip_acl.nil?
-      @ip_acl_partial = "create_ipacl"
+      @ip_acl_partial = 'create_ipacl'
     else
-      @ip_acl_partial = "ipacl"
+      @ip_acl_partial = 'ipacl'
       @ip_acl = @client.account.sip.ip_access_control_lists.get(@agent.ip_acl)
     end
 
     if @agent.auth_acl.nil?
-      @acl_partial = "create_acl"
+      @acl_partial = 'create_acl'
     else
-      @acl_partial = "acl"
-      @auth_acl = @client.account.sip.credential_lists.get(@agent.auth_acl) 
+      @acl_partial = 'acl'
+      @auth_acl = @client.account.sip.credential_lists.get(@agent.auth_acl)
     end
-
-    @agent_name = current_user.name
-
-    capability = Twilio::Util::Capability.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    capability.allow_client_incoming current_user.id
-   @token = capability.generate()
   end
 
+  def capability_token
+    capability = Twilio::Util::Capability.new(
+      Rails.application.secrets.twilio_account_sid,
+      Rails.application.secrets.twilio_auth_token)
+    capability.allow_client_incoming current_user.id
+    capability.generate
+  end
+
+  def create_rest_client
+    Twilio::REST::Client.new(
+      Rails.application.secrets.twilio_account_sid,
+      Rails.application.secrets.twilio_auth_token)
+  end
 end
